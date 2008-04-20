2e03df45b7a9a6840c86d62f0c08ad1c6648993c
helio <helio@cd0f7e8c-89fa-11dd-a14b-9ba93c563a2f>
2008-04-20 17:06:11 +0000

    * add stat and dirname for the new mounter.
    * fixed small mounter bug.

--- trunk/src/build.sh
+++ trunk/src/build.sh
@@ -51,7 +51,7 @@ mdmfs mkdir more mount moused mv nc newfs pciconf ping powerd ps pwd
 pwd_mkdb reboot rm route sed sh sha1 sha256 ssh ssh-keygen sshd stty 
 swapon swapoff swapinfo syslogd tail tar tcsh tftp tftpd top umount 
 uniq unlink usbdevs vidcontrol whoami zcat sort pfctl du makefs 
-mount_msdosfs getextattr setextattr devinfo newfs_msdos
+mount_msdosfs getextattr setextattr devinfo newfs_msdos stat dirname
 "
 
 # List of kernel objects in base that will be on the image. Ports modules
