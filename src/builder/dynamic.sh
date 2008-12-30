89dbf751a5cc3e9df0d6a8296bb5fbd9b7f7e677
heliocentric <heliocentric@cd0f7e8c-89fa-11dd-a14b-9ba93c563a2f>
2008-12-30 20:41:53 +0000

    * Add support for manually copying the device.hints... for some reason (which escapes me) make distribution is not moving it into place, and neither is installworld. Did anyone ever bother to double check their MFCs?.

--- src/builder/dynamic.sh
+++ src/builder/dynamic.sh
@@ -62,6 +62,8 @@ mkdir -p ${DESTDIR}/rescue 1>&2
 mkdir -p ${DESTDIR}${BOOTPATH}/defaults
 priv make distribution 1>&2
 priv make installworld 1>&2
+mkdir -p ${DESTDIR}/boot
+cp ${DESTDIR}/usr/src/sys/${TARGET_ARCH}/conf/GENERIC.hints ${DESTDIR}/boot/device.hints
 priv make INSTKERNNAME=${KERNCONF} installkernel 1>&2
 mkdir -p ${DESTDIR}/usr/src
 echo "				[DONE]"
