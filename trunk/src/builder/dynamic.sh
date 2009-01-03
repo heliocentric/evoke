8ce4b05d47fce54b3e35b4fd9334034d68df3219
heliocentric <heliocentric@cd0f7e8c-89fa-11dd-a14b-9ba93c563a2f>
2009-01-03 05:12:50 +0000

    * Ok, I've found the problem. Recent FreeBSD -CURRENT snapshots no longer had kse; but 6.4 uses kse by default. That's why not all ports using threading bombed out, only those using configure. Let's force the image to only use libthr.

--- trunk/src/builder/dynamic.sh
+++ trunk/src/builder/dynamic.sh
@@ -94,6 +94,13 @@ if [ "${BUILD_PORTS}" != "" ] ; then
 		fi
 	fi
 	
+	# Work around the fact that 8-CURRENT no longer has kse, by forcing 6.x to use libthr
+	if [ "${ABI}" = "6" ] ; then
+		ln -f ${DESTDIR}/usr/lib/libthr.so.2 ${DESTDIR}/usr/lib/libc_r.so.5
+		ln -f ${DESTDIR}/usr/lib/libthr.so.2 ${DESTDIR}/usr/lib/libc_r.so.6
+		ln -f ${DESTDIR}/usr/lib/libthr.so.2 ${DESTDIR}/usr/lib/libpthread.so.1
+		ln -f ${DESTDIR}/usr/lib/libthr.so.2 ${DESTDIR}/usr/lib/libpthread.so.2
+	fi
 	# For ports, uname will return the correct values
 	UNAME_r=${RELEASE} UNAME_m=${TARGET_ARCH} UNAME_p=${TARGET_ARCH} chroot ${DESTDIR} /portbuild.sh 1>&2
 	rm ${DESTDIR}/libexec/ld-elf32.so.1
