2d5a8c0fa498d95e826d5531dfebcb8ed9f989e0
heliocentric <heliocentric@cd0f7e8c-89fa-11dd-a14b-9ba93c563a2f>
2009-01-04 05:24:31 +0000

    * change the version in mkisofs.

--- src/build.sh
+++ src/build.sh
@@ -30,7 +30,7 @@ export i386_ACTIVE="6.4-RELEASE/i386"
 
 # Evoke Version
 export VERSION="HEAD"
-export REVISION="$(svnversion)"
+export REVISION="r$(svnversion)"
 
 # Release Engineer
 export ENGINEER="Dylan Cochran"
