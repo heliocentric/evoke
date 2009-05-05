717f26a3887f5fe116ae3b9139ea6853cbba3a08
heliocentric <heliocentric@cd0f7e8c-89fa-11dd-a14b-9ba93c563a2f>
2009-05-05 19:37:24 +0000

    * Make sure we patch out %%ABI%% and %%ARCH%%.

--- trunk/builder/build.sh
+++ trunk/builder/build.sh
@@ -188,6 +188,12 @@ do
 	cd ${BUILDDIR}/targets/FreeBSD/share/
 	tar -cf - nsrc | tar -xf - -C ${DESTDIR}/usr/src/
 
+	cd ${DESTDIR}/usr/src/nsrc
+	FILELIST="$(find . -not -path \*.svn\* -not -type d)"
+
+	sed -i .bak 's@%%ABI%%@FreeBSD-${RELEASE}@g' ${FILELIST}
+	sed -i .bak 's@%%ARCH%%@${TARGET}@g' ${FILELIST}
+
 	# Allow us to build both statically and dynamically. Note, it needs to be automated more.
 
 	case "${1}" in
