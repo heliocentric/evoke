b2b8aeae5bd0948c093fdc36288ed2b9aa2c8fbd
heliocentric <heliocentric@cd0f7e8c-89fa-11dd-a14b-9ba93c563a2f>
2009-05-05 19:08:56 +0000

    * Add setctty and some of the headers.

--- trunk/builder/targets/FreeBSD/share/nsrc/nexusd/nexusd.c
+++ trunk/builder/targets/FreeBSD/share/nsrc/nexusd/nexusd.c
@@ -1,1 +1,34 @@
+#include <sys/param.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <libutil.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <stdarg.h>
+#include <sys/mount.h>
+#include <sys/uio.h>
 
+int setctty(const char *);
+
+int main() {
+	if (getpid() == 1) {
+
+	}
+	/* How the hell did we get here? */
+	return 1;
+}
+
+int setctty(const char *name) {
+        int fd;
+
+        revoke(name);
+        if ((fd = open(name, O_RDWR)) == -1) {
+                return 1;
+        }
+        if (login_tty(fd) == -1) {
+                return 1;
+        } else {
+		return 0;
+	}
+}
